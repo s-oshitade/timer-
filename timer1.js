f7023c6a8a6e206ebb9215e86e7444ad0af14f1d
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-19 17:53:17 +0000

    Add function alarmFxn

--- timer1.js
+++ timer1.js
@@ -1,3 +1,13 @@
+//Purpose: Function alarmFxn converts positive numbers from CLI to system beep sounds in milli seconds.
+const alarmArray = process.argv.slice(2);
+const alarmFxn = function(alarms) {
+  for(const alarm of alarms) {
+    if(alarm > 0 && alarm !== NaN){
+      setTimeout(() => {
+      process.stdout.write('\x07');
+    }, alarm * 1000); //convert input data from secs to milli secs
+    }
+  } 
+}
 
-
-
+alarmFxn(alarmArray);
